a0e2dd2b96c562c0ac475faaed1e01db6343b3e6
dulmaa@gobicashmere.com <dulmaa@gobicashmere.com>
2024-11-07 11:48:59 +0800

    update

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -5,8 +5,8 @@ import Spline from "@splinetool/react-spline"
 
 export default function Experience() {
   return (
-    <section className="max-container h-[100vh]">
-        <Spline scene="https://prod.spline.design/jY4qTTYu2vwih-BJ/scene.splinecode" />
+    <section className="max-container-next h-[100vh]">
+        <Spline scene="https://prod.spline.design/CS2Qz6eT1rvrWXKk/scene.splinecode" />
         </section>
   );
 }
